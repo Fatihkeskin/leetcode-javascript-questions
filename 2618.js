973481c3d0cd77905847767ecf876cdc49b01b42
Fatih Keskin <fatihkeskin7@outlook.com>
2023-07-20 16:21:54 +0200

    id = 2618, difficulty = medium

---
+++ 2618.js
@@ -0,0 +1,25 @@
+/**
+ * 2618. Check if Object Instance of Class
+ * @param {any} obj
+ * @param {any} classFunction
+ * @return {boolean}
+ */
+const checkIfInstanceOf = (obj, classFunction) => {
+    if (obj === null || obj === undefined || typeof classFunction !== 'function') {
+        return false; 
+    }
+
+    let prototype = Object.getPrototypeOf(obj); 
+    while (prototype) {
+        if (prototype === classFunction.prototype) {
+            return true;
+        }
+        prototype = Object.getPrototypeOf(prototype); 
+    }
+
+    return false; 
+};
+
+/**
+ * checkIfInstanceOf(new Date(), Date); // true
+ */
